3d720fdf85aafdf8592210b41c294409e2880f66
JJ Baird <jj@biomasterglobal.com>
2017-04-21 16:37:00 -0400

    Picture changes for system discrepancies

--- BM.Boulder.IDlinkInterface/Controls/ResultPage.xaml.cs
+++ BM.Boulder.IDlinkInterface/Controls/ResultPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Drawing;
+using System.IO;
 using System.Threading;
 using System.Windows.Media.Imaging;
 using BM.Boulder.IDlinkInterface.Properties;
+using BM.DataAccess;
 using Brushes = System.Windows.Media.Brushes;
 
 namespace BM.Boulder.IDlinkInterface.Controls
@@ -18,15 +21,21 @@ namespace BM.Boulder.IDlinkInterface.Controls
         private readonly MainWindow _mainForm;
 
 
-        public ResultPage(string memberId, string name, MainWindow mainForm)
+        public ResultPage(Member member, MainWindow mainForm)
         {
             InitializeComponent();
             
             StatusLabel.Content = "Success";
-            MemberIdLabel.Content = memberId;
-            NameLabel.Content = name;
-            StatusBorder.Background = Brushes.Green;           
-            StatusImage.Source = new BitmapImage(new Uri($"pack://application:,,,{CorrectImage}"));
+            MemberIdLabel.Content = member.MemberNumber;
+            NameLabel.Content = $"{member.FirstName} {member.LastName}";
+            StatusBorder.Background = Brushes.Green;
+            var image = LoadImage(member);
+            StatusImage.Source = image == null ? new BitmapImage(new Uri($"pack://application:,,,{CorrectImage}")) : image;
+            if (image != null)
+            {
+                StatusImage.Width = image.Width;
+                StatusImage.Height = image.Height;
+            }
 
             _mainForm = mainForm;
             _backgroundWorker.DoWork += BackgroundWorker_DoWork;
@@ -40,7 +49,7 @@ namespace BM.Boulder.IDlinkInterface.Controls
             StatusLabel.Content = "Member was not found";
             MemberIdLabel.Content = "";
             NameLabel.Content = "";
-            StatusBorder.Background = Brushes.Red;
+            StatusBorder.Background = Brushes.White;
             StatusImage.Source = new BitmapImage(new Uri($"pack://application:,,,{NotFoundImage}"));
 
             _mainForm = mainForm;
@@ -48,6 +57,52 @@ namespace BM.Boulder.IDlinkInterface.Controls
             _backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
             _backgroundWorker.RunWorkerAsync();
         }
+
+        private BitmapImage LoadImage(Member member)
+        {
+            if  (member.Photo == null || (member.Photo.ImageType == ImageTypeEnum.XMLSerialization && member.Photo.XMLSerialization.BMPBytes.Length == 0)) return null;
+            if (member.Photo.ImageType == ImageTypeEnum.Path && !File.Exists(member.Photo.FileName)) return null;
+            
+            var image = new BitmapImage();
+            switch (member.Photo.ImageType)
+            {
+                case ImageTypeEnum.None:
+                    return null;
+                case ImageTypeEnum.XMLSerialization:
+                    using (var mem = new MemoryStream(member.Photo.XMLSerialization.BMPBytes))
+                    {
+                        mem.Position = 0;
+                        image.BeginInit();
+                        image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                        image.CacheOption = BitmapCacheOption.OnLoad;
+                        image.UriSource = null;
+                        image.StreamSource = mem;
+                        image.EndInit();
+                    }
+                    image.Freeze();
+                    
+                    break;
+                case ImageTypeEnum.Path:
+                    using (var fs = File.Open(member.Photo.FileName, FileMode.Open))
+                    {
+                        var dImg = new Bitmap(fs);
+                        var ms = new MemoryStream();
+                        dImg.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        var bImg = new BitmapImage();
+                        bImg.BeginInit();
+                        bImg.StreamSource = new MemoryStream(ms.ToArray());
+                        bImg.EndInit();
+                        //img is an Image control.                        
+                        image = bImg;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            return image;                        
+        }
+
+
 
         private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
--- BM.Boulder.IDlinkInterface/Controls/UserInputPage.xaml.cs
+++ BM.Boulder.IDlinkInterface/Controls/UserInputPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using BM.Boulder.IDlinkInterface.Properties;
@@ -13,7 +14,7 @@ namespace BM.Boulder.IDlinkInterface.Controls
     public partial class UserInputPage
     {
         private readonly MainWindow _mainForm;
-        private readonly BackgroundWorker _backgroundWorker = new BackgroundWorker();
+        private BackgroundWorker _backgroundWorker;
 
         public UserInputPage(MainWindow mainForm)
         {
@@ -28,24 +29,29 @@ namespace BM.Boulder.IDlinkInterface.Controls
             if (e.Key != Key.Enter) return;
             if (TbMemberId.Text == "") return;
 
-            Member member = null;
+            Member member = null;            
+            _mainForm.Content = new LoadingPage();
+            _backgroundWorker = new BackgroundWorker();
             _backgroundWorker.DoWork += (o, args) =>
-            {
+            {                
                 var dataAccess = new CTCData(Settings.Default.Connection);
-                member = dataAccess.GetMemberByMemberId(TbMemberId.Text);
+                member = dataAccess.GetMemberByMemberId(args.Argument.ToString());
             };
             _backgroundWorker.RunWorkerCompleted += (o, args) =>
             {
                 if (args.Error != null)
                 {
+                    _mainForm.SelectPageUserInputPage();
                     MessageBox.Show($"{args.Error.Message}{Environment.NewLine}{Environment.NewLine}{args.Error}");
                     return;                    
                 }
+                Thread.Sleep(200);
                 _mainForm.Content = member == null
                     ? new ResultPage(_mainForm)
-                    : new ResultPage(member.MemberNumber, $"{member.FirstName} {member.LastName}", _mainForm);
+                    : new ResultPage(member, _mainForm);
             };
-            _backgroundWorker.RunWorkerAsync();
+            //User ID is input to the background thread
+            _backgroundWorker.RunWorkerAsync(TbMemberId.Text);
         }
     }
 }
